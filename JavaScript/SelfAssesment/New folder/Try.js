7203c86544c7c25ca0d3122303970d32e9fa4610
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-01-04 18:32:34 +0530

    updated try

--- JavaScript/SelfAssesment/New folder/Try.js
+++ JavaScript/SelfAssesment/New folder/Try.js
@@ -1,49 +1,85 @@
 
 
-// debugger;
-// function abcd(){
+// // debugger;
+// // function abcd(){
 
-//     return function xyz(){
-//         console.log("Hello World");
-//     }
-// }
+// //     return function xyz(){
+// //         console.log("Hello World");
+// //    
+// // }
 
-// var val = abcd();
+// // var val = abcd();
 
-// val();
-
-
-// function savings(amount){
-
-// let bal=amount;
-// console.log(`your balance is ${bal}`);
+// // val();
 
 
+// // function savings(amount){
 
-//     function fixedDeposit(years){
-//         let year = years;
-//         console.log(`your balance is ${bal} and  fixed deposit year is ${year}`);
-//     }
-
-
-//     return fixedDeposit(12);
-
-// }
+// // let bal=amount;
+// // console.log(`your balance is ${bal}`);
 
 
-function parent(){
 
-     let money =80000;
- console.log(money);
+// //     function fixedDeposit(years){
+// //         let year = years;
+// //         console.log(`your balance is ${bal} and  fixed deposit year is ${year}`);
+// //     }
+
+
+// //     return fixedDeposit(12);
+
+// // }
+
+
+// function parent(){
+
+//      let money =80000;
+//  console.log(money);
  
-    function child(){
-        let savings=1000;
-        console.log(savings+money);//! money is accessed due to the clouser property
+//     function child(){
+//         let savings=1000;
+//         console.log(savings+money);//! money is accessed due to the clouser property
         
-    }
+//     }
 
-    return child();
-}
+//     return child();
+// }
 
-let val=parent();
-console.log(val);
+// let val=parent();
+// console.log(val);
+
+
+// //somehthing is importannt here i dont understand if 
+// return function hello(){
+//     count++;
+//     console.log("hello brother i am here how are you");
+//     console.log(count);
+// }
+// return count; // This line never executes
+
+// function greet(){
+
+//     let count=0;
+//     return function hello(){
+
+//         count++;
+//         console.log("hello brother i am here how are you");
+// console.log(count);
+        
+    
+//     }
+//    return count; 
+
+// }
+
+// let a=greet();
+// console.log(a);
+// a();
+// a();
+// a();
+// console.log(a);
+
+// let b=greet();
+// b();
+
+
