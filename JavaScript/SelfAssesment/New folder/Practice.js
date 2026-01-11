ee146c59e6f62507bdc83c65aaf78232e2ddb813
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-01-11 12:50:31 +0530

    practice added

---
+++ JavaScript/SelfAssesment/New folder/Practice.js
@@ -0,0 +1,34 @@
+
+
+// function operation(a,b,op){
+//     return op(a,b);
+// }
+
+// function op(a,b){
+//     return a+b
+// }
+
+// let ans=operation(10,32,op);
+// console.log(ans);
+
+let a=(a=1,b="1")=> a==b;
+
+
+
+console.log(a());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
