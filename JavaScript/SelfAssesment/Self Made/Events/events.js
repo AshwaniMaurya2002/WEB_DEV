5a90a04d3b7a07f01e430eb2d8493876fc0349a8
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-01-21 22:07:47 +0530

    .

--- JavaScript/SelfAssesment/Self Made/Events/events.js
+++ JavaScript/SelfAssesment/Self Made/Events/events.js
@@ -1,15 +1,38 @@
-let h1Tag=document.createElement("h1")
-h1Tag.textContent="hello"
-h1Tag.style.backgroundColor="cyan"
-h1Tag.style.textAlign="center"
-h1Tag.style.border="solid"
-h1Tag.style.borderWidth="10px"
-h1Tag.style.padding="20px"
-h1Tag.style.marginTop="50%"
+// let h1Tag=document.createElement("h1")
+// h1Tag.textContent="hello"
+// h1Tag.style.backgroundColor="cyan"
+// h1Tag.style.textAlign="center"
+// h1Tag.style.border="solid"
+// h1Tag.style.borderWidth="10px"
+// h1Tag.style.padding="20px"
+// h1Tag.style.marginTop="50%"
 
 
 
-let pTag=document.createElement("p")
-pTag.innerHTML="loream"
-console.log(h1Tag);
-document.body.appendChild(h1Tag)
+// let pTag=document.createElement("p")
+// pTag.innerHTML="loream"
+// console.log(h1Tag);
+
+// document.body.appendChild(h1Tag)
+
+
+function formEvent(e){
+    e.preventDefault();
+    console.log("form submitted");
+
+    let user={
+        name: e.target[0].value,
+        pass:e.target[1].value,
+    }
+
+    console.log(` welcome ${user.name}  pass ${user.pass}`);
+
+    alert("welceome user")
+}
+
+
+
+
+
+
+
