0677e6bb747c1cf4d568b1a82cb9bf39a76696af
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-01-31 17:49:00 +0530

    mock updated

---
+++ Self_Assesment/JavaScript/mock.js
@@ -0,0 +1,25 @@
+
+
+
+// let obj={
+//     id:1,
+//     name:"rohan",
+//     pin:2452,
+//     address:{
+//         road:1,
+//         area:31,
+//         street:63
+//     }
+
+// }
+
+
+
+
+setTimeout((e) => {
+
+    console.log("gsgs");
+    
+}, 2000);
+
+
---
+++ Self_Assesment/JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+
+let arr=[12,24,3,4,2,4,5]
+
+let val=arr.reduce((acc,ele)=>{
+    return acc+ele;
+});
+
+console.log(val);
