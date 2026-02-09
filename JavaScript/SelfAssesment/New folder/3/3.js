a23ef0853d20af402db4ed6141bc4811b6d03826
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-02-09 21:48:08 +0530

    update practice

---
+++ JavaScript/SelfAssesment/New folder/3/3.js
@@ -0,0 +1,12 @@
+
+const divTag=document.createElement("div")
+
+divTag.innerHTML="<p><i><del>Lorem ipsum</p>"
+document.body.append(divTag)
+divTag.style.fontSize="43px"
+divTag.style.color="red"
+divTag.style.border="10px solid black"
+divTag.style.textAlign="center"
+
+
+
