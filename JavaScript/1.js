d34c2b02a3e364c1c2c763eef496633aef3bb99f
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-03-26 14:42:48 +0530

    commit

--- JavaScript/1.js
+++ JavaScript/1.js
@@ -1,4 +1,47 @@
 
-let arr3=[10,20,30,40,50]
-let val3=arr3.map(element=>element>=30)
-console.log(val3);
+
+// const cart=[
+//     {
+//         item:"Lays",
+//         price:20,
+//         category:"snacks",
+//         qty:2
+
+//     },
+//     {
+//         item:"Lays",
+//         price:20,
+//         category:"snacks",
+//         qty:2
+//     },
+
+//         {
+//         item:"Lays",
+//         price:40,
+//         category:"snacks",
+//         qty:1
+//         },
+//         {
+//         item:"Lays",
+//         price:78,
+//         category:"snacks",
+//         qty:3
+//         },
+
+// {
+//         item:"Lays",
+//         price:41,
+//         category:"snacks",
+//         qty:1}
+        
+// ]
+
+
+
+// const x=cart.reduce((acc,element)=>{
+//     acc=acc+element.price*element.qty;
+//     return acc;
+// })
+// console.log(x);
+
+
