677d9a2a4250b872a477a543f3b21d8a61145a6a
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-04-06 18:27:50 +0530

    made new Project

---
+++ React/02_React/vite-project/src/components/UserCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import boy from "../assets/boy.webp"
+
+const UserCard = () => {
+  return (
+    <div>
+      <h1 className='heading'>Ashwani Maurya</h1>
+        <img src={boy} alt="error" />
+        <p> Hellow  this is me started learning the react js</p>
+
+    </div>
+  )
+}
+
+export default UserCard
