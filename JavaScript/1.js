feaaed02efca697f08daa033731160226b375efe
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-04-15 19:57:50 +0530

    maded the path

--- JavaScript/1.js
+++ JavaScript/1.js
@@ -2,9 +2,14 @@
 
 const x=async()=>{
 
-    const response=await fetch('https://jsonplaceholder.typicode.com/todos');
+    const response=await fetch('https://jsonplaceholder.typicode.com/photos');
     const data=await response.json();
     console.log(data);
 
 
 }
+
+x();
+
+function display(){
+    co
