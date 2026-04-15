fab00680873871deee3cf115b88ebbf5f205e290
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-04-15 20:02:23 +0530

    done with the code

--- JavaScript/1.js
+++ JavaScript/1.js
@@ -5,11 +5,25 @@ const x=async()=>{
     const response=await fetch('https://jsonplaceholder.typicode.com/photos');
     const data=await response.json();
     console.log(data);
-
-
+     
+    display(data);
 }
 
-x();
 
-function display(){
-    co
+
+function display(data){
+    data.forEach((element) => {
+        let div=document.createElement("div");
+        div.setAttribute("class","card");   
+
+        let img=document.createElement("img");
+        img.src=element.url;
+
+        let title=document.createElement("h3");
+        title.innerText=element.title;  
+
+        body.append(div);
+        div.append(img,title);
+    });
+}
+    
