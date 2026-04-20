3b75a3cddaa1f035a65e6027660b044106399352
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-04-20 19:51:21 +0530

    code pushed

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,8 +1,14 @@
 
-export const App=()=>{
+
+//?this is a component
+ const App=()=>{
 
   return <h1>Hello from React</h1>
 }
 
+const heading= <h1>Hello my friend</h1>
+
+
+export default App;
 
 
--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {App} from './App.jsx'
+import App from './App.jsx'
+
+import heading from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
