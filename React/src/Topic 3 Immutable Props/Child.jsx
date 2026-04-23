92715b88b2ed202968198a2583f22e8c3fd083b0
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-04-23 17:36:17 +0530

    push

---
+++ React/src/Topic 3 Immutable Props/Child.jsx
@@ -0,0 +1,8 @@
+
+
+const Child=(props)=>{
+
+    console.log(props)
+}
+
+export default Child;
---
+++ React/src/Topic 3 Immutable Props/Parent.jsx
@@ -0,0 +1,14 @@
+
+
+import Child from "./Child";
+
+const Parent=()=>{
+
+const username="sid"
+const age=24;
+
+    return <div>
+
+<Child  username={username} age={age}/>
+    </div>
+}
