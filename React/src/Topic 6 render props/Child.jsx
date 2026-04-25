9a7e9e927d2b48a0b7823c86ab49930f73537ff0
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-04-25 18:41:44 +0530

    new commit

--- React/src/Topic 6 render props/Child.jsx
+++ React/src/Topic 6 render props/Child.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Child = ({ render = "HELLO" }) => {
-
     // if you pass value then the function will render or other wise 
   return (
     <div>{typeof render == "function" ? render("Rohit Sharma") : render}</div>
