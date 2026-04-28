889deaa6566506ac78bf9a68692f99f271b587ef
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-04-28 18:57:38 +0530

    deleted the commited statement

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,6 +1,3 @@
-// import Card from "./Topic 1 JSX/Card"
-// import Parent from "./Topic 5 children prop/Parent";
-// import Parent from "./Topic 6 render props/Parent";
 import Card from "./Topic 7 synthetic events/Card";
 
 //?this is a component
