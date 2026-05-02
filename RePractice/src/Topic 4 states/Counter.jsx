f4922150e723bd8d97073f9bd32fd8750d27fb0f
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-02 18:05:57 +0530

    counter

--- RePractice/src/Topic 4 states/Counter.jsx
+++ RePractice/src/Topic 4 states/Counter.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Counter = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
-      
+      <h1>Counter is:{count}</h1>
+      <button onClick={() => setCount(count + 1)}>
+        Click to increase count
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
