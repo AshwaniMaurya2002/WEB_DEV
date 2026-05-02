231286e5d6d77dcd330673d023c7a056ddd7c3dd
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-02 18:05:43 +0530

    done

--- RePractice/src/App.jsx
+++ RePractice/src/App.jsx
@@ -1,15 +1,12 @@
-
-
-
 // import Card from "./Topic 1 Jsx/Card";
 // import Card from "./Topic 2 Props/Card";
 // import Parent from "./Topic 2 Props/Parent";
-import Parent from "./Topic 3 Immutable and map function/Parent";
+// import Parent from "./Topic 3 Immutable and map function/Parent";
 
-const App=()=>{
+import Counter from "./Topic 4 states/Counter";
 
-    return  <Parent/>
-
-}
+const App = () => {
+  return <Counter />;
+};
 
 export default App;
