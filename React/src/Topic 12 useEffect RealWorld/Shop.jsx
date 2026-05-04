c887c891ffae7796023a0ac80bebc065185063b8
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-04 20:28:25 +0530

    product

--- React/src/Topic 12 useEffect RealWorld/Shop.jsx
+++ React/src/Topic 12 useEffect RealWorld/Shop.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Shop = () => {
 
     const[response ,setResponse]=useState([]);
 
+    useEffect(()=>{
 
-    
+        const getProducts=async()=>{
+
+            const response=
+        }
+
+
+
+    },[])
+
+
+
 
 
   return <div></div>;
