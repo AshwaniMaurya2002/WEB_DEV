aaea3884137b20a8eae4368ad96134e3e47a2aa2
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-07 19:27:29 +0530

    pushed

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,11 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+// pages
+import App from "./App";
+import Home from "./Topic-14 React Routing/Home";
+import Profile from "./Topic-14 React Routing/Profile";
+import Shop from "./Topic-14 React Routing/Shop";
+import Order from "./Topic-14 React Routing/Order";
 
+// Routing configuration
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "shop",
+        element: <Shop />,
+      },
+      {
+        path: "order",
+        element: <Order />,
+      },
+    ],
+  },
+]);
 
-      <App/>
-
-
-)
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={appRouter} />,
+);
