b14f7c37a81fb253a8713bc4f9e4d0b29b3cf51f
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-08 21:44:43 +0530

    .

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 import { Outlet } from "react-router-dom";
-import Header from "./Topic 14 React Routing/Header";
-
+import Header from "./Topic 14 React Routing/Header"
 const App = () => {
   return (
     <div id="app">
