44410fd055036b9460e2c08422502902faf27584
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-10 11:20:37 +0530

    udpate the app jsx

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Topic 14 React Routing/components/Header";
-
 const App = () => {
   return (
     <div id="app">
