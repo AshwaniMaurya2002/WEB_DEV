cc87d733162bf51fe5083db1f4644e5ec86cebf5
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-13 21:26:22 +0530

    udpate the productpage

--- React/src/Topic 14 React Routing/ProductPage.jsx
+++ React/src/Topic 14 React Routing/ProductPage.jsx
@@ -1,11 +1,24 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 const ProductPage = () => {
   const [singleProduct, setSingleProduct] = useState(null);
+  const [count, setCount] = useState(1);
   const [loading, setLoading] = useState(false);
-  const { id } = useParams();
-  console.log("id:", id);
+  const { productId } = useParams();
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("category"));
+  console.log(searchParams.get("page"));
+
+  const changeSearchParams = () => {
+    setSearchParams((prev) => {
+      prev.set("category", "fragrance");
+      prev.set("page", "10");
+      prev.set("limit", "10");
+      return prev;
+    });
+  };
 
   useEffect(() => {
     const controller = new AbortController(); // step-1 controller creation
@@ -13,10 +26,13 @@ const ProductPage = () => {
     const fetchProducts = async () => {
       try {
         setLoading(true);
-        const response = await fetch(`https://dummyjson.com/products/${id}`, {
-          cache: "no-store",
-          signal: controller.signal, // step-2 signal
-        });
+        const response = await fetch(
+          `https://dummyjson.com/products/${productId}`,
+          {
+            cache: "no-store",
+            signal: controller.signal, // step-2 signal
+          },
+        );
 
         if (!response.ok) throw new Error("Somthing went wrong");
         const data = await response.json();
@@ -41,9 +57,11 @@ const ProductPage = () => {
     };
   }, []);
 
-  if (loading) {
+  if (loading || !singleProduct) {
     return <h1>Loading Products.....</h1>;
   }
+
+  const { title, tags, price, description } = singleProduct;
 
   return (
     <div className="product-page">
@@ -54,10 +72,30 @@ const ProductPage = () => {
           </div>
         </div>
         <div className="right">
-          {/* <p className="category">{category}</p>
           <div className="title">{title}</div>
-          <div className="description">{customizeDescription}</div>
-          <div className="price">&#x24; {price}</div> */}
+          <div className="description">{description}</div>
+          <div className="tags-container">
+            {tags.map((tag, idx) => (
+              <span key={idx} className="tags">
+                {tag}
+              </span>
+            ))}
+          </div>
+          <div className="price">&#x24; {price}</div>
+
+          <div className="cart-container">
+            <div>tax included. Shipping calculated at checkout</div>
+            <div className="qty-container">
+              <button onClick={() => setCount(count > 1 ? count - 1 : count)}>
+                -
+              </button>
+              <span>{count}</span>
+              <button onClick={() => setCount(count + 1)}>+</button>
+            </div>
+            <button className="add-cart" onClick={changeSearchParams}>
+              Add To Cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
@@ -65,3 +103,10 @@ const ProductPage = () => {
 };
 
 export default ProductPage;
+
+/**
+ * ! useTransition
+ * ! useDefferedValue
+ *
+ * !! Tanstack (Boss)
+ */
