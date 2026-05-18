112bd7de2b82341bc0e22407d371ceb217eede7e
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-18 23:05:47 +0530

    made changes in main and other folder

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,15 +1,25 @@
 import { createRoot } from "react-dom/client";
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// pages
 import App from "./App";
-import Home from "./Topic 14 React Routing/Home";
-import Profile from "./Topic 14 React Routing/Profile";
-import Shop from "./Topic 14 React Routing/Shop";
-import ProtectedRoute from "./Topic 14 React Routing/routes/ProtectedRoute";
-import Order from "./Topic 14 React Routing/Order";
-import ProductPage from "./Topic 14 React Routing/ProductPage";
-import NotFound from "./Topic 14 React Routing/NotFound";
-import Login from "./Topic 14 React Routing/Login";
+import Home from "./Topic-14 React Routing/pages/Home";
+import Profile from "./Topic-14 React Routing/pages/Profile";
+import Shop from "./Topic-14 React Routing/pages/Shop";
+import Order from "./Topic-14 React Routing/pages/Order";
+import NotFound from "./Topic-14 React Routing/pages/NotFound";
+import ProductPage from "./Topic-14 React Routing/pages/ProductPage";
+
+// auth pages
+import Signup from "./Topic-14 React Routing/pages/Signup";
+
+// Routes
+import ProtectedRoute from "./Topic-14 React Routing/routes/ProtectedRoute";
+
+// apis
+import { getUserProfile } from "./Topic-14 React Routing/services/api";
+import ErrorPage from "./Topic-14 React Routing/pages/ErrorPage";
 
 // Routing configuration
 const appRouter = createBrowserRouter([
@@ -27,6 +37,8 @@ const appRouter = createBrowserRouter([
           {
             path: "profile",
             element: <Profile />,
+            loader: getUserProfile,
+            errorElement: <ErrorPage />,
           },
           {
             path: "shop",
@@ -45,8 +57,8 @@ const appRouter = createBrowserRouter([
     ],
   },
   {
-    path: "/login",
-    element: <Login />,
+    path: "/signup",
+    element: <Signup />,
   },
   {
     path: "*",
