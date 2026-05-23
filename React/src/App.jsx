89ad897a7f5158c900c9bc68693f8ceacb6cbaf3
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-05-23 19:23:36 +0530

    calculator program

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Header from "./Topic 14 React Routing/components/Header";
+import Header from "./Topic-14 React Routing/components/Header";
+
 const App = () => {
+  const user = { username: "chombu", job: "Fullstack Dev" };
+
   return (
     <div id="app">
       <Header />
 
-      <Outlet />
+      <Outlet context={user} />
     </div>
   );
 };
--- React/src/Topic-14 React Routing/services/api.js
+++ React/src/Topic-14 React Routing/services/api.js
@@ -1,5 +1,7 @@
 export const getUserProfile = async () => {
-  const response = await fetch("https://api.github.com/usersss/sarvesh-1999");
+  const response = await fetch(
+    "https://api.github.com/users/AshwaniMaurya2002",
+  );
 
   if (!response.ok) throw new Error("404 Something went wrong");
 
